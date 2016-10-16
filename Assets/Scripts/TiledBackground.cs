f7aac32793ee99eaab2234b54b1f50773a1b09b5
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-16 13:20:50 -0200

    03 Animate repeat background textures

--- Assets/Scripts/TiledBackground.cs
+++ Assets/Scripts/TiledBackground.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class TiledBackground : MonoBehaviour {
 	public int textureSize = 32;
+	public bool scaleHorizontally = true;
+	public bool scaleVertically = true;
 
 	// Use this for initialization
 	void Start () {
-		var newWidth = Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
-		var newHeight = Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
+		var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
+		var newHeight = !scaleVertically ? 1 : Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
 
 		transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1);
 

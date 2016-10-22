fec806dc774f8ae78bee0d326e13e8a936732f57
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-22 11:16:27 -0200

    02 Move obstacles

---
+++ Assets/InstantVelocity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantVelocity : MonoBehaviour {
+	public Vector2 velocity = Vector2.zero;
+	private Rigidbody2D body2d;
+
+	void Awake()
+	{
+		body2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		body2d.velocity = velocity;
+	}
+}
